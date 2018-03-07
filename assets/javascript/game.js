f0afa3aefe88956ccf0dce78f067411b0f721609
Paul Park <park.paul.94@gmail.com>
2018-03-06 21:14:54 -0500

    made a function to initialize the game upon button click

---
+++ assets/javascript/game.js
@@ -0,0 +1,67 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    var randomInt = Math.floor(Math.random() * (max - min)) + min;
+    return randomInt;
+}//The maximum is exclusive and the minimum is inclusive
+
+function getRandGuess (min, max, array) {
+    var index = getRandomInt (min, max);
+    var str = array[index];
+    return str;
+    
+}
+
+const wins = "#wincount";
+const losses = "#losscount";
+const wrong = "#wrongguesses";
+const blanks = "#words";
+const remaining = "#guessesremaining";
+const easy = "#easy";
+const medium = "#medium";
+const hard = "#hard";
+
+var gameBoardArray = [];
+var correctGuess = [];
+var numSpaces = [];
+var scores = [];
+var highScore = 0;
+
+var easyGame = {
+    numWins: 0,
+    numLosses: 0,
+    cpuGuess: [],
+    maxGuesses: 5,
+    gameArray: ["zap", "job", "dam", "dog", "cat", "win", "get", "zip", "vex", "jog", 
+                "fox", "fax", "pod", "pad", "zoo", "zit", "van", "dig", "cup", "gum", "dad", "mom",
+                "lax", "joy", "tax", "bat", "rat", "did", "wax", "mix", "fix", "man", "ran", "box"],
+
+initializeGame: function() {
+    $(wins).text(0);
+    $(losses).text(0);
+    $(wrong).text("none");
+    $(blanks).text(0);
+    $(remaining).text(0);
+    this.cpuGuess = getRandGuess(0, this.gameArray.length, this.gameArray);
+    console.log(this.cpuGuess);
+},
+
+playAgain: function() {
+    $(wrong).text("none");
+    $(blanks).text(0);
+    $(remaining).text(0);
+}
+};
+
+$(document).ready(function() {
+    $(easy).click(function() {
+        easyGame.initializeGame();
+        let bull = confirm('play again');
+        if (bull) {
+            easyGame.playAgain();
+        }
+        else {
+            easyGame.initializeGame();
+        }
+})
+});
